4d7f59a723b0c871312a60e2d4f4eea8a49d26c7
Raziel <razielmelchor@gmail.com>
2021-02-23 22:10:11 -0600

    add activity dashboard

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Divider, Grid, List } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
+import ActivityDetails from '../details/ActivityDetails';
 import ActivityList from './ActivityList';
 
 
@@ -12,8 +13,13 @@ export default function ActivityDashboard({activities}: Props){
     return(
         <Grid>
             <Grid.Column width='10'>
-            <ActivityList activities={activities}/>
+                <ActivityList activities={activities}/>
             </Grid.Column>
+            <Grid.Column width='6'>
+                {activities[0] && 
+                <ActivityDetails activity={activities[0]}/>}
+            </Grid.Column>
+
         </Grid>
     );
 }
--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { Card, Image , Icon, Button} from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
 
-export default function ActivityDetails(){
-    return ();
+interface Props{
+    activity: Activity;
+}
+
+export default function ActivityDetails({activity}: Props){
+    return (
+        <Card fluid>
+            <Image src={`/assets/categoryImages/${activity.category}.jpg`} wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>Matthew</Card.Header>
+                <Card.Meta>
+                    <span className='date'>Joined in 2015</span>
+                </Card.Meta>
+                <Card.Description>
+                    {activity.description}
+                </Card.Description>
+                </Card.Content>
+            <Card.Content extra>
+                <Button.Group widths='2'>
+                    <Button basic color='blue' content='Edit'/>
+                    <Button basic color='grey' content='Cancel'/>
+                </Button.Group>
+            </Card.Content>
+        </Card>
+
+    );
 }
