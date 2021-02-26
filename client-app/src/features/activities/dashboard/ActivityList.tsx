adade391320c5884899106ca006107f75dac5525
Raziel <razielmelchor@gmail.com>
2021-02-25 18:24:31 -0600

    added delete functionality through mobx

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,23 +1,19 @@
+import { observer } from 'mobx-react-lite';
 import React, { SyntheticEvent, useState } from 'react';
 import { Button, Item, List, Segment, Label } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 
-interface Props{
-    activities: Activity[];
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
-
-export default function ActivityList({activities, deleteActivity, submitting} : Props){
+export default observer( function ActivityList(){
     const [target, setTarget] = useState('');
+
+    const {activityStore} = useStore();
+    const {activities, loading} = activityStore;
 
     function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string){
         setTarget(e.currentTarget.name);
-        deleteActivity(id);
+        activityStore.deleteActivity(id);
     }
-
-    const {activityStore} = useStore();
 
     return(
         <Segment>
@@ -36,7 +32,7 @@ export default function ActivityList({activities, deleteActivity, submitting} : 
 
                                 <Button
                                     name={activity.id}
-                                    loading={submitting && target === activity.id}
+                                    loading={loading && target === activity.id}
                                     onClick={(e) => handleActivityDelete(e, activity.id)}
                                     floated='right'
                                     content='Delete'
@@ -51,4 +47,4 @@ export default function ActivityList({activities, deleteActivity, submitting} : 
         </Segment>
 
     );
-}
+})
