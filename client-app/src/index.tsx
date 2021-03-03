a7e55e303d3aea765d16067a4cbdd7d10424e7be
Raziel <razielmelchor@gmail.com>
2021-03-02 19:41:53 -0600

    added formik validation

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
 import { store, StoreContext } from './app/stores/store';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css'
 import {createBrowserHistory} from 'history';
